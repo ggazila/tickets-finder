11e5f7bfbed4c1dbfef93792aedddfc7c9ffe989
Bohdan Kulinich <kulia@macpaw.com>
2023-07-08 00:19:10 +0300

    feat: telegram report 2

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -89,7 +89,8 @@ test('find a talons', async ({ page }) => {
         // @ts-ignore
         // if(marker?.cnt && marker?.offices_n === '4641') {
         if(marker?.cnt && marker?.offices_n === '8049') {
-          results.push(`🚗ТСЦ #: ${marker?.offices_n} \nДата: ${date?.text.toString().toUpperCase()} Талончиків: ${marker?.cnt} 🚗\n`);
+          const dateText = date?.text.toString().toUpperCase().replace(/\n/g, '');
+          results.push(`🚗ТСЦ #: ${marker?.offices_n} Дата: ${dateText} Талончиків: ${marker?.cnt} 🚗`);
           // @ts-ignore
           console.log(`🚗 🚗ТСЦ #: ${marker?.offices_n} Талончиків: ${marker?.cnt}  🚗 🚗 🚗\n`)
         }
@@ -98,7 +99,7 @@ test('find a talons', async ({ page }) => {
   }
 
   if(results && results.length > 0) {
-    writeFileSync('results.txt', results.join('\n'));
+    writeFileSync('results.txt', results.join(' '));
   }
 
 
